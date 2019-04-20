677d205072231900cc5171d3a9f1eb49263ef2c5
w-rudolph <172247097@qq.com>
2019-04-20 23:03:53 +0800

    update

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,7 +1,53 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Req,
+  HttpException,
+  UseGuards,
+} from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { StatusCode } from '../../common/error-status';
+import { Comment } from './comment.entity';
+import { AuthGuard as JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('/admin/comment')
 export class CommentController {
+  constructor(private commentService: CommentService) {}
+
   @Get('list')
-  actionGetList() {}
+  async actionGetList(@Query('postId') postId) {
+    postId = Number(postId);
+    if (!postId) {
+      throw new HttpException('无效postId', StatusCode.FAIL);
+    }
+    return await this.commentService.getCommentList(postId);
+  }
+
+  @Post('add')
+  @UseGuards(JwtAuthGuard)
+  async actionAdd(
+    @Req() req,
+    @Body('postId') postId: number,
+    @Body('toUser') toUser: number,
+    @Body('content') content: string,
+  ) {
+    postId = Number(postId);
+    const userId = req.currentUser && req.currentUser.id;
+    toUser = Number(toUser) || 0;
+    if (!postId) {
+      throw new HttpException('无效postId', StatusCode.FAIL);
+    }
+    if (!content) {
+      throw new HttpException('无效评论', StatusCode.FAIL);
+    }
+    const comment = new Comment();
+    comment.postId = postId;
+    comment.userId = userId;
+    comment.toUser = toUser;
+    comment.content = content;
+    return await this.commentService.addComment(comment);
+  }
 }
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, FindConditions } from 'typeorm';
+import { Comment } from './comment.entity';
 @Injectable()
 export class CommentService {
-  getCommentList() {
-    return [];
+  constructor(
+    @InjectRepository(Comment)
+    private commentRef: Repository<Comment>,
+  ) {}
+
+  async getCommentList(postId: number) {
+    const req = await this.commentRef
+      .createQueryBuilder('comment')
+      .where({ postId })
+      .leftJoin('user', 'u1', 'u1.id = comment.userId')
+      .leftJoin('user', 'u2', 'u2.id = comment.toUser')
+      .select(['comment.*', 'u1.name as userName', 'u2.name as toUserName']);
+
+    return req.getRawMany();
+  }
+
+  addComment(comment: Comment) {
+    return this.commentRef.save(comment);
   }
 }
